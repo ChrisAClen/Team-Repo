4239d14a84ec4bd90bd28b709f795a8861655950
Christopher Clendinen <chrisaclendinen@gmail.com>
2019-01-29 15:19:13 -0500

    added web Client

---
+++ eMNY/eMNY.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eMNY.Client.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
