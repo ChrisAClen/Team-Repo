a67d46037815088472ca35aff2831f48efa59506
Edmond Thompson <edmond.j.thompson@gmail.com>
2019-02-04 19:26:14 -0500

    Card validation

--- eMNY/eMNY.Domain/Models/Card.cs
+++ eMNY/eMNY.Domain/Models/Card.cs
@@ -1,14 +1,24 @@
 using eMNY.Domain.Abstracts;
+using eMNY.Domain.Interfaces;
 using System;
 
 namespace eMNY.Domain.Models
 {
-  public class Card
+  public class Card : AThing
   {
     public long CardNumber { get; set; }
     public int SecurityNumber { get; set; }
     public Account Account { get; set; }
     public int Pin { get; set; }
     public DateTime ExpirationDate { get; set; }
+
+    public override bool IsValid()
+    {
+      return 
+      Validator.ValidateNumber(this) &&
+      Account.IsValid() &&
+      (ExpirationDate != null);
+
+    }
   }
 }
