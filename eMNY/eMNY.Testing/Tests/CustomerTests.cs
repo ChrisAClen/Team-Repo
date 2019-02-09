9c4504add995c4743a9c3d2b0dfa3c08465634d5
Narely Najera <nnarely@yahoo.com>
2019-02-09 16:42:53 -0500

    changes to tests

--- eMNY/eMNY.Testing/Tests/CustomerTests.cs
+++ eMNY/eMNY.Testing/Tests/CustomerTests.cs
@@ -16,6 +16,24 @@ namespace eMNY.Testing.Tests
         Password = "aPassword",
         Email = "jsmith@email.com"
       };
+
+      Address = new Address()
+      {
+        Street = "3230 Banyan Cir",
+        City = "Tampa",
+        StateProvince = "FL",
+        PostalCode = "33613"
+      };
+
+      Card = new Card()
+      {
+        CardNumber = 1234123412341234,
+        SecurityNumber = 123,
+        Pin = 1234
+        //ExpirationDate = 
+
+      };
+
     }
 
     [Fact]
@@ -25,6 +43,15 @@ namespace eMNY.Testing.Tests
       Assert.IsType<string>(sut.UserName);
       Assert.IsType<string>(sut.Password);
       Assert.IsType<string>(sut.Email);
+      Assert.IsType<Address>(sut.Address);
+      Assert.IsType<string>(sut.Street);
+      Assert.IsType<string>(sut.City);
+      Assert.IsType<string>(sut.StateProvince);
+      Assert.IsType<string>(sut.PostalCode);
+      Assert.IsType<Card>(sut.Card);
+      Assert.IsType<long>(sut.CardNumber);
+      Assert.IsType<int>(sut.SecurityNumber);
+      Assert.Isype<int>(sut.Pin);
     }
   }
 }
