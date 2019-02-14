147d420ff1a99c30229da942733c6929b8dfb866
Narely Najera <nnarely@yahoo.com>
2019-02-13 21:18:49 -0500

    working on accounttests- [#163844654}

---
+++ eMNY/eMNY.Testing/Tests/AccountTests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using eMNY.Domain.Models;
+using Xunit;
+
+namespace eMNY.Testing.Tests
+{
+  public class AccountTests
+  {
+    private readonly Account sut;
+
+    public AccountTests()
+    {
+      sut = new Account()
+      {
+        AccountNumber = 2111234567,
+        CreationDate = DateTime.Now,
+        InterestRate = 17.05m,
+        Amount = 16780.50m
+
+      };
+    }
+
+    [Fact]
+
+    public void AccountTest()
+    {
+      Assert.IsType<int>(sut.AccountNumber);
+      Assert.IsType<DateTime>(sut.CreationDate);
+      Assert.IsType<Decimal>(sut.InterestRate);
+      Assert.IsType<Decimal>(sut.Amount);
+    }
+
+  }
+}
