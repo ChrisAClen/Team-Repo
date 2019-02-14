2670883c93f81541ac82cd70c2dc080650e81d3b
Christopher Clendinen <chrisaclendinen@gmail.com>
2019-02-14 09:34:21 -0500

    adjustments to account

--- eMNY/eMNY.Domain/Models/Account.cs
+++ eMNY/eMNY.Domain/Models/Account.cs
@@ -67,7 +67,7 @@ namespace eMNY.Domain.Models
     public int CreateAccountNumber()
     {
       Random rnd = new Random();
-      int lead = 711;
+      int lead = 211;
       int RandNumb1 = rnd.Next(0000000, 9999999);
 
       AccountNumber = int.Parse(lead.ToString() + RandNumb1.ToString());
